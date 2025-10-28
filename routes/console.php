a4eac5604fe80842769c0b97549a9fa616b23a1c
unknown <atefdiabmohamed@gmail.com>
2025-10-29 00:55:23 +0200

    Task Scheduling into

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::call(function () {
+
+    DB::table('recent_users')->delete();
+
+})->daily();
+
+
